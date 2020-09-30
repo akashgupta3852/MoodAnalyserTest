d3ebf5b04815ffc66ec3d2c4754bb803d1b8c10c
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-30 23:32:23 +0530

    [Akash]Added: welcome message

---
+++ src/main/java/com/bridgelabz/MoodAnalyserTest/MoodAnalyser.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.MoodAnalyserTest;
+
+public class MoodAnalyser 
+{
+    public void printWelcome()
+    {
+        System.out.println( "Welcome to Mood Analyser Problem" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest/MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.MoodAnalyserTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MoodAnalyser.
+ */
+public class MoodAnalyserTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
